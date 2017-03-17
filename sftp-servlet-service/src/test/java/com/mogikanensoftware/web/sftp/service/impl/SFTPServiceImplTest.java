7dff12112738bb318bd36407ea8f62fb31071871
Vladislav V <Vladislav V>
2017-03-16 21:20:54 -0400

    Initial testing configuration
    - Unit and Mockito
    - TestCase skeleton for SFTPServiceImplTest

---
+++ sftp-servlet-service/src/test/java/com/mogikanensoftware/web/sftp/service/impl/SFTPServiceImplTest.java
@@ -0,0 +1,70 @@
+package com.mogikanensoftware.web.sftp.service.impl;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class SFTPServiceImplTest {
+
+	@Ignore
+	@Test
+	public void testSFTPServiceImpl() {
+		fail("Not yet implemented");
+	}
+
+	@Ignore
+	@Test
+	public void testAddIdentity() {
+		fail("Not yet implemented");
+	}
+
+	@Ignore
+	@Test
+	public void testGetProxyForHost() {
+		fail("Not yet implemented");
+	}
+
+	@Ignore
+	@Test
+	public void testDisconnect() {
+		fail("Not yet implemented");
+	}
+
+	@Ignore
+	@Test
+	public void testChangeDirectory() {
+		fail("Not yet implemented");
+	}
+
+	@Ignore
+	@Test
+	public void testUploadFileFileString() {
+		fail("Not yet implemented");
+	}
+
+	@Ignore
+	@Test
+	public void testUploadFileStringStringString() {
+		fail("Not yet implemented");
+	}
+
+	@Ignore
+	@Test
+	public void testConnect() {
+		fail("Not yet implemented");
+	}
+
+	@Ignore
+	@Test
+	public void testReconnect() {
+		fail("Not yet implemented");
+	}
+
+	@Ignore
+	@Test
+	public void testIsConnected() {
+		fail("Not yet implemented");
+	}
+
+}
